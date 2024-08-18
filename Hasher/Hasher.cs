5862e9b439376063c4d5ba12ef6bd201645df2fb
Krupperman <master-gsm@inbox.ru>
2024-08-18 22:21:14 +0300

    Добавьте файлы проекта.

---
+++ Hasher/Hasher.cs
@@ -0,0 +1,66 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Hasher
+{
+    public static class HasherSha256
+    {     
+        static string GetSalt()
+        {
+            return (Guid.NewGuid()).ToString();
+        }
+        /// <summary>
+        /// Compute hash of string with salt guid and using sha256 algoritm
+        /// </summary>
+        /// <param name="myString">string</param>
+        /// <returns>returns two strings- item1: hashedValue, item2: salt </returns>
+        public static Tuple<string,string> ComputeWithSalt(string myString)
+        { 
+            string salt = GetSalt();
+            myString += salt;
+            // Create a SHA256   
+            using SHA256 sha256Hash = SHA256.Create();
+            for (int i = 0; i < 1000; i++)
+            {
+
+
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(myString));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new();
+                for (int j = 0; j < bytes.Length; j++)
+                {
+                    builder.Append(bytes[j].ToString("x2"));
+                }
+                myString = builder.ToString();
+            }
+            return Tuple.Create(myString, salt);
+        }
+        /// <summary>
+        /// Compute hash of string using sha256 algoritm
+        /// </summary>
+        /// <param name="myString">string</param>
+        /// <returns> type string, parametr hashedValue </returns>
+        public static string Compute(string myString)
+        {
+            using SHA256 sha256Hash = SHA256.Create();
+            for (int i = 0; i < 1000; i++)
+            {
+
+
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(myString));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new();
+                for (int j = 0; j < bytes.Length; j++)
+                {
+                    builder.Append(bytes[j].ToString("x2"));
+                }
+                myString = builder.ToString();
+            }
+            return myString;
+        }
+    }
+}
---
+++ Tasks/Program.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+using System.Text;
+using System.Security.Cryptography;
+using Hasher;
+
+namespace HashConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("-------------------------------------------------------------------------");
+                Console.Write($"Input password: ");
+                string? myString = Console.ReadLine();
+                if(String.IsNullOrEmpty(myString)|| myString.Length < 6)
+                {
+                    Console.WriteLine("Error:Password must have at list  6 characters!");
+                    Console.WriteLine("-------------------------------------------------------------------------");
+                }
+                else
+                {
+                    var data = Hasher.HasherSha256.ComputeWithSalt(myString);
+                    Console.WriteLine($"HashString: {myString}");
+                    Console.WriteLine($"Salt:{data.Item2}");
+                    Console.WriteLine($"Hashed string:{data.Item1}");
+                }
+            }
+            
+        }
+       
+         
+    }
+
+}
